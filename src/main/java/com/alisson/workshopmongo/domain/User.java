a2a8ee96affa86e278f193af9889bd4ff3d9f421
Alisson Silva <alisson.s.dias@hotmail.com>
2020-09-17 15:30:06 -0300

    connect

--- src/main/java/com/alisson/workshopmongo/domain/User.java
+++ src/main/java/com/alisson/workshopmongo/domain/User.java
@@ -2,27 +2,38 @@ package com.alisson.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String name;
 	private String email;
 	
 	public User() {}
 
-	public User(String id, String name, String email) {
+	public User(Long id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
---
+++ src/main/java/com/alisson/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.alisson.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alisson.workshopmongo.domain.User;
+import com.alisson.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+}
